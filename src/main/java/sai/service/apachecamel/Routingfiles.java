c218390c26f4bdf928f421e1a06e98b1c8571362
stg <coddinginjava@gmail.com>
2019-09-01 10:42:48 +0530

    no change

--- src/main/java/sai/service/apachecamel/Routingfiles.java
+++ src/main/java/sai/service/apachecamel/Routingfiles.java
@@ -13,11 +13,11 @@ public class Routingfiles extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("file://{{user.home}}/Desktop/in?noop=false&recursive=true&include=.*\\.txt&")
-//                .filter( exchange ->  exchange.getIn().getBody(File.class).)
+        from("file://{{user.home}}/Desktop/in?noop=false&recursive=true&include=.*\\.txt")
                 .routeId("executeFileComponent")
                 .to("file://{{user.home}}/Desktop/out/txt");
 
+        //http://www.javanbeyond.com/camel/file-operations-in-camel-ftpfile-components/
         //if its .txt file it has to go into {{user.home}}/Desktop/out/txt
         //if its .mp3 file it has to go into {{user.home}}/Desktop/out/mp3
 
